2beb4fde3175ebbec16fe0f8cb7ccedd2db5a948
yibarr <yael@os.city>
2020-10-30 20:33:53 -0600

    hash on passwords

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,7 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const SALT_WORK_FACTOR = 10;
 
 const userSchema = new Schema({
   is_active: {
@@ -39,6 +42,20 @@ const userSchema = new Schema({
 }, {
   timestamps: true,
   versionKey: false,
+});
+
+// eslint-disable-next-line consistent-return
+userSchema.pre('save', async function (next) {
+  try {
+    const user = this;
+    if (!user.isModified('password')) return next();
+    const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+    const hash = await bcrypt.hash(user.password, salt);
+    user.password = hash;
+    return next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 const User = model('User', userSchema, 'Users');
